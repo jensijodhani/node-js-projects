ef221e115d7cc25171b511a2c5b15561de92c588
jensijodhani <jensijodhani2043@gmail.com>
2024-11-27 10:26:04 +0530

    node project

--- PR-8(admin panel)/index.js
+++ PR-8(admin panel)/index.js
@@ -12,25 +12,6 @@ const path = require('path');
 
 app.use('/',express.static(path.join(__dirname,'/public')))
 
-const passport = require('passport');
-
-const passportLocal = require('./config/passportLocal');
-
-const session = require('express-session');
-
-app.use(session({
-    secret: 'rnw3',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24
-    }
-}))
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(passport.setUser);
-
 app.use(express.urlencoded());
 
 app.use('/', (require('./routes/indexRoutes')));
